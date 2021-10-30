cd37c8e28a0fd77637faaf5a95c5337ecd27e001
JoseBorbon <joserborbon@gmail.com>
2021-10-30 18:32:26 -0400

    edit component

--- src/DadJoke.jsx
+++ src/DadJoke.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import './DadJoke.css';
 
 class DadJoke extends Component {
   render() {
-    return <div>Yolo</div>;
+    return (
+      <div className="Dad-joke">
+        <div className="Dad-joke-rating">
+          <button>⬆</button>
+          <div>{this.props.rating}</div>
+          <button>⬇</button>
+        </div>
+
+        <div>{this.props.joke}</div>
+        <img
+          className="Dad-joke-emoji"
+          src="https://www.citypng.com/public/uploads/preview/smiling-face-emoji-emoticon-eye-heart-red-love-11583591506qpben5gjej.png"
+        />
+      </div>
+    );
   }
 }
 
