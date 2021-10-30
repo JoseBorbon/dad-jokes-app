6177deba1ca1df8cd6aa0ccbc35a201254db6dd2
JoseBorbon <joserborbon@gmail.com>
2021-10-30 07:11:21 -0400

    add componentDidMount and render dadJokes

--- src/DadJokes.jsx
+++ src/DadJokes.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DadJoke from './DadJoke';
 
+const URL = 'https://icanhazdadjoke.com/search?limit=10'; //<- 10 dad jokes fetch
+
 class DadJokes extends Component {
-  constructor() {}
+  constructor(props) {
+    super(props);
+    this.state = { rankings: [] };
+  }
+
+  async componentDidMount() {
+    const jokes = await axios.get(URL, {
+      headers: { Accept: 'application/json' },
+    });
+    this.setState((st) => {
+      return {
+        rankings: jokes.data.results.map(({ joke, id }) => {
+          return { id, joke, rating: 0 };
+        }),
+      };
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <DadJoke />
-      </div>
-    );
+    const dadJokes = this.state.rankings.map(({ joke, id, rating }) => {
+      return <DadJoke key={id} id={id} joke={joke} rating={rating} />;
+    });
+    return <div>{dadJokes}</div>;
   }
 }
 
