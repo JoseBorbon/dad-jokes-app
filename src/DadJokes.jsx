c8095ceba031e5d4e627a46c6c306c635ffcbd4b
JoseBorbon <joserborbon@gmail.com>
2021-10-30 11:02:41 -0400

    add getJokes method

--- src/DadJokes.jsx
+++ src/DadJokes.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import DadJoke from './DadJoke';
+import getRandPage from './utilities/getRandPage';
 
-const URL = 'https://icanhazdadjoke.com/search?limit=10'; //<- 10 dad jokes fetch
+const BASE_URL = 'https://icanhazdadjoke.com/search';
 
 class DadJokes extends Component {
   constructor(props) {
     super(props);
-    this.state = { rankings: [] };
+    this.state = { rankings: [], uniquePages: new Set([1]) };
+    this.getJokes = this.getJokes.bind(this);
   }
 
   async componentDidMount() {
-    const jokes = await axios.get(URL, {
+    const jokes = await axios.get(BASE_URL + '?limit=10', {
       headers: { Accept: 'application/json' },
     });
     this.setState((st) => {
@@ -23,11 +25,49 @@ class DadJokes extends Component {
     });
   }
 
+  async getJokes() {
+    const { uniquePages } = this.state;
+    try {
+      if (uniquePages.size === 64) {
+        throw new Error('No more jokes available from API');
+      }
+      let randPage;
+      do {
+        randPage = getRandPage(65, 1);
+      } while (uniquePages.has(randPage));
+
+      const newJokes = await axios.get(
+        BASE_URL + `?page=${randPage}&limit=10`,
+        {
+          headers: { Accept: 'application/json' },
+        }
+      );
+
+      this.setState((st) => {
+        return {
+          rankings: [
+            ...st.rankings.slice(),
+            ...newJokes.data.results.map(({ joke, id }) => {
+              return { id, joke, rating: 0 };
+            }),
+          ],
+          uniquePages: st.uniquePages.add(randPage),
+        };
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
   render() {
     const dadJokes = this.state.rankings.map(({ joke, id, rating }) => {
       return <DadJoke key={id} id={id} joke={joke} rating={rating} />;
     });
-    return <div>{dadJokes}</div>;
+    return (
+      <div>
+        {dadJokes}
+        <button onClick={this.getJokes}>New Jokes!</button>
+      </div>
+    );
   }
 }
 
