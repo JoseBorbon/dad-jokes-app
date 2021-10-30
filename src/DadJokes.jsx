3481a18e781cd2942c6286e769417f93a57eaa06
JoseBorbon <joserborbon@gmail.com>
2021-10-30 06:19:41 -0400

    add DadJokes component

---
+++ src/DadJokes.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DadJoke from './DadJoke';
+
+class DadJokes extends Component {
+  constructor() {}
+  render() {
+    return (
+      <div>
+        <DadJoke />
+      </div>
+    );
+  }
+}
+
+export default DadJokes;
