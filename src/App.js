3557a9b1df60db2a965ed457b3d917c0a17a5e59
JoseBorbon <joserborbon@gmail.com>
2021-10-30 18:33:06 -0400

    add container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <DadJokes />
+      <div className="Dad-jokes-container">
+        <DadJokes />
+      </div>
     </div>
   );
 }
