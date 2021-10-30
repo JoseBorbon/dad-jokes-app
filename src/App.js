e540dd87d87e83fd27f839217036c2a6c489da26
JoseBorbon <joserborbon@gmail.com>
2021-10-30 06:19:04 -0400

    base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import DadJoke from './DadJoke';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DadJoke />
+    </div>
+  );
+  //https://icanhazdadjoke.com/search?limit=10 <- 10 dad jokes fetch
 }
 
 export default App;
