73387daa05c352d8332c2baddf41f2a47fbb600c
JoseBorbon <joserborbon@gmail.com>
2021-10-31 17:58:00 -0400

    add emojis data file

---
+++ src/data/emojis.js
@@ -0,0 +1,10 @@
+const emojis = [
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/rolling-on-the-floor-laughing_1f923.png',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/grinning-squinting-face_1f606.png',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/grinning-face-with-big-eyes_1f603.png',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/slightly-smiling-face_1f642.png',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/confused-face_1f615.png',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/pouting-face_1f621.png',
+];
+
+export default emojis;
