330403a9f4826390e60a95ad4107dca4c4bec5e3
JoseBorbon <joserborbon@gmail.com>
2021-10-31 18:00:15 -0400

    add circleRating to improve readability

--- src/DadJoke.jsx
+++ src/DadJoke.jsx
@@ -2,32 +2,24 @@ import React, { Component } from 'react';
 import './DadJoke.css';
 
 class DadJoke extends Component {
-  //default props for all the images we'll use and store in array and based off rating can assign index pos
   handleChangeRating(incOrDecBy1) {
     this.props.changeRating(this.props.id, incOrDecBy1);
   }
 
   render() {
-    const { joke, rating } = this.props;
+    const { joke, emojiLink, rating } = this.props;
+    const circleRating = `Dad-joke-rating-circle ${
+      rating >= 8 ? 'Dad-joke-great' : rating > 3 ? 'Dad-joke-okay' : ''
+    }`;
     return (
       <div className="Dad-joke-container">
         <div className="Dad-joke-rating">
           <button onClick={() => this.handleChangeRating(+1)}>⬆</button>
-          <div
-            className={`Dad-joke-rating-circle ${
-              rating >= 8 ? 'Dad-joke-great' : rating > 3 ? 'Dad-joke-okay' : ''
-            }`}
-          >
-            {rating}
-          </div>
+          <div className={circleRating}>{rating}</div>
           <button onClick={() => this.handleChangeRating(-1)}>⬇</button>
         </div>
-
         <div>{joke}</div>
-        <img
-          className="Dad-joke-emoji"
-          src="https://www.citypng.com/public/uploads/preview/smiling-face-emoji-emoticon-eye-heart-red-love-11583591506qpben5gjej.png"
-        />
+        <img className="Dad-joke-emoji" src={emojiLink} />
       </div>
     );
   }
